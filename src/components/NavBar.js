ecac33bcab356fdc965ed40ac922b6e18999afa8
Cipta <ordinarypeople67@gmail.com>
2024-11-15 23:15:46 +0700

    added link linekdln

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -82,7 +82,7 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a
-                  href="https://www.linkedin.com/in/diaz-eka-cipta-206755221/"
+                  href="https://www.linkedin.com/in/diazekacipta/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
